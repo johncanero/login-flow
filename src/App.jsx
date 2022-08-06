520e6dbcca6af0fee6cbab7e34fc84c41d0f9707
John Canero <johncanero.official@gmail.com>
2022-08-06 13:01:06 +0800

    Conditional Rendering Lesson with the Ternary Operator and && Operator

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import Login from './components/Login';
+
+
+
+const App = () => {
+
+  var isLoggedIn = false;
+
+  const currentTime = new Date(2022, 8, 6, 13).getHours();
+  console.log(currentTime)
+
+
+  return (
+    <div className='container'>{ 
+      /* isLoggedIn === true ? <h1>Hello</h1> : <Login />   */
+
+       currentTime > 12 ? <h1>Why are you still working?</h1> : null
+
+      /* currentTime > 12 && <h1>Why are you still working?</h1> */
+
+    }</div>
+  )
+}
+
+
+export default App;
+
+
