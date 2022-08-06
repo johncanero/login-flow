8fab65b25ca8a9acd48385da1cfec94e2cecce4f
John Canero <johncanero.official@gmail.com>
2022-08-06 15:03:47 +0800

    Update Interface

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ const App = () => {
 
   var isLoggedIn = false;
 
-  const currentTime = new Date(2022, 8, 6, 13).getHours();
-  console.log(currentTime)
+  // const currentTime = new Date(2022, 8, 6, 13).getHours();
+  // console.log(currentTime)
 
 
   return (
     <div className='container'>{ 
-      /* isLoggedIn === true ? <h1>Hello</h1> : <Login />   */
+       isLoggedIn === true ? <h1>Hello</h1> : <Login />  
 
       /* currentTime > 12 ? <h1>Why are you still working?</h1> : null */
 
-      currentTime > 12 && <h1>Why are you still working?</h1>
+      /* currentTime > 12 && <h1>Why are you still working?</h1> */
 
     }</div>
   )
