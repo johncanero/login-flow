90354683be77be32f806821e8a400d0a25688770
John Canero <johncanero.official@gmail.com>
2022-08-06 18:02:39 +0800

    Ternary Operator and Props Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import './App.css';
-import Login from './components/Login';
+import Form from './components/Form';
 
 
 
 const App = () => {
 
-  var isLoggedIn = false;
-
-  // const currentTime = new Date(2022, 8, 6, 13).getHours();
-  // console.log(currentTime)
-
+  var userIsRegistered = true;
 
   return (
     <div className='container'>{ 
-       isLoggedIn === true ? <h1>Hello</h1> : <Login />  
-
-      /* currentTime > 12 ? <h1>Why are you still working?</h1> : null */
-
-      /* currentTime > 12 && <h1>Why are you still working?</h1> */
-
+        <Form 
+          isRegistered={userIsRegistered}
+        />
     }</div>
   )
 }
--- src/components/Input.js
+++
@@ -1,9 +0,0 @@
-const Input = (props) => {
-  return (
-    <div>
-        <input type={props.type} placeholder={props.placeholder} />
-    </div>
-  )
-}
-
-export default Input
